18b12bcc8afbd9bd09a494dcc7b56d5161454a43
thefakeweed <sql.leb@gmail.com>
2017-06-01 15:56:59 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var add = require('./add.js');
+var substract = require('./substract.js');
+var multiply = require ('./multiply.js');
+var divide = require ('./divide.js');
+var exponentiation = require('./exponentiation.js');
+
+console.log(add(100,20));
+
+console.log(substract(1000,900));
+
+console.log(multiply(10,10));
+
+console.log(divide(100,2));
+
+console.log(exponentiation(4,3));
